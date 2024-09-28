b65f9579ecf973c4d963b6e175d7017cc7bd1abe
Jesus <normales01@gmail.com>
2024-09-27 19:23:54 -0500

    FEAT: Adición de la función crear articulo
    Se agrego la función de  crear  articulo lo que hace es primero comprobar si el método es post en caso de ser de esta forma usa los campos del formulario  y en caso de que sea valido, lo guarda y envía,  y luego nos redirige a la lista de los artículos.

---
+++ noticias/views.py
@@ -0,0 +1,14 @@
+
+from django.shortcuts import render, redirect
+from .form import ArticleForm
+
+def create_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')  # Redirige a una vista que liste los artículos
+    else:
+        form = ArticleForm()
+    
+    return render(request, 'vista_inicial.html', {'form': form})
