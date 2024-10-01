083be088834a902371dd3f2175812dc7d85a80ec
Jesus <normales01@gmail.com>
2024-10-01 07:30:59 -0500

    FEAT: Adición de Rutas
    Se agregaron las rutas para la parte del login y del menú.

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import create_article
+from .views import create_article,login,menu
+
 
 urlpatterns = [
     path('create/', create_article, name='create_article'),
      #path('list/', views.ArticleListView.as_view(), name='article_list'),
+     path('login/',login , name='login'),
+     path('menu/',menu , name='menu'),
+
 ]
