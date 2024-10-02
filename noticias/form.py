0e6f5ff4edb59c9d7dbaa3119caf64fae2676167
Jesus <normales01@gmail.com>
2024-10-02 17:48:03 -0500

    FIX: Corrección del login
    Al solicitar inicio de sesión se pedía correo en vez de el username,  se corrigió editando el form default de django.

--- noticias/form.py
+++ noticias/form.py
@@ -1,7 +1,24 @@
 from django import forms
 from .models import Article
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'content', 'author', 'category'] 
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
